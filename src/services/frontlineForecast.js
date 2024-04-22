5d5e3b8ee6e3d3541513e171b773d753b6b3185a
Dustbowl <vuonganthony@hotmail.com>
2024-04-22 04:17:49 -0500

    changed dependency to momentjs

--- src/services/frontlineForecast.js
+++ src/services/frontlineForecast.js
@@ -1,1 +1,23 @@
-import date from 'date-and-time';
+import moment from 'moment';
+
+const frontlineMaps = ['Shatter', 'Onsal Hakair', 'Seal Rock'];
+const anchorDate = moment.utc('2024-04-20 15:00:00', 'YYYY-MM-DD HH:mm:ss'); //Shatter Anchor Date, GMT 15:00:00 is 00:00:00 in JST which is daily reset time
+
+export function parseForecastTime(forecastDate = 'NOTHING') {
+    if (forecastDate === 'NOTHING') {
+        //var forecast = _getForecast(moment());
+    } else {
+        var forecast;
+    }
+    return forecast;
+}
+
+function _getForecast(forecastDate){
+    return _getFrontlineMap(forecastDate);
+}
+
+function _getFrontlineMap(forecastDate) {
+    var timeDifference = Math.floor(forecastDate.diff(anchorDate, 'days'));
+    console.log(timeDifference);
+    return frontlineMaps[timeDifference%3];
+}
