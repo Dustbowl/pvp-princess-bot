400b73976e74201ad430cdbe6637d1eec93053a8
Dustbowl <vuonganthony@hotmail.com>
2024-04-22 23:19:43 -0500

    added forecast functions

--- src/services/frontlineForecast.js
+++ src/services/frontlineForecast.js
@@ -1,23 +1,61 @@
 import moment from 'moment';
 
-const frontlineMaps = ['Shatter', 'Onsal Hakair', 'Seal Rock'];
-const anchorDate = moment.utc('2024-04-20 15:00:00', 'YYYY-MM-DD HH:mm:ss'); //Shatter Anchor Date, GMT 15:00:00 is 00:00:00 in JST which is daily reset time
+const frontlineMaps = ['Fields of Glory (Shatter)', 'Onsal Hakair (Danshig Nadaam)', 'Seal Rock (Seize)'];
+const anchorDate = moment('2024-04-21 00:00:00 +0900', 'YYYY-MM-DD HH:mm:ss ZZ'); //Shatter Anchor Date, GMT 15:00:00 is 00:00:00 in JST which is daily reset time
 
 export function parseForecastTime(forecastDate = 'NOTHING') {
     if (forecastDate === 'NOTHING') {
-        //var forecast = _getForecast(moment());
+        var forecast = _getForecast();
     } else {
-        var forecast;
+        var forecast = _getForecast(moment('2024-04-26 10:00:00', 'YYYY-MM-DD HH:mm:ss'), false);
     }
     return forecast;
 }
 
-function _getForecast(forecastDate){
-    return _getFrontlineMap(forecastDate);
-}
+function _getForecast(forecastDate = moment(), current = true){
+    var forecast = "```";
+    var timeDifference = forecastDate.diff(anchorDate, 'days');
+    
+    if (current) {
+        var temp = moment(anchorDate).add(timeDifference, 'days');
+        for (let i = 0; i < frontlineMaps.length; i++) {
+            var line = "";
+            
+            if(i == 0)  {
+                line = line.concat(">>").padEnd(3).padStart(4);
+                line = line.concat(frontlineMaps[(timeDifference + i) % frontlineMaps.length].padEnd(40), "now");
+            }
+            else {
+                line = line.concat("").padStart(4);
+                line = line.concat(frontlineMaps[(timeDifference + i) % frontlineMaps.length].padEnd(40));
+                temp.add(1, 'days');   
+                var timeTil = moment.duration(temp.diff(forecastDate));
+                line = line.concat( timeTil.get('days'), "d ",
+                                    timeTil.get('hours'), "h ",
+                                    timeTil.get('minutes'), "m ");
 
-function _getFrontlineMap(forecastDate) {
-    var timeDifference = Math.floor(forecastDate.diff(anchorDate, 'days'));
-    console.log(timeDifference);
-    return frontlineMaps[timeDifference%3];
+            }
+            forecast = forecast.concat(line, "\n");
+        }
+    } else {
+        var temp = moment(anchorDate).add(timeDifference - frontlineMaps.length, 'days');
+        for (let i = frontlineMaps.length - 1; i >= 0 ; i--) {
+            var line = "";
+            if(i == 0)  {
+                line = line.concat(">>").padEnd(3).padStart(4);
+            } else {
+                line = line.concat("").padStart(4);
+            }
+            line = line.concat(frontlineMaps[(timeDifference - i) % frontlineMaps.length].padEnd(40));
+            temp.add(1, 'days');
+            var timeTil = moment.duration(temp.diff(moment()));
+            line = line.concat( timeTil.get('days'), "d ",
+                                timeTil.get('hours'), "h ",
+                                timeTil.get('minutes'), "m ");
+            forecast = forecast.concat(line, "\n");
+        }
+
+    }
+    forecast = forecast.concat("```");
+    return forecast;
 }
