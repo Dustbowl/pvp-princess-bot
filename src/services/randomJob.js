997bb7b2780296bd44d3a688ce839c8f380f4fdf
Dustbowl <vuonganthony@hotmail.com>
2024-04-27 03:55:57 -0500

    added !job command functionality

--- src/services/randomJob.js
+++ src/services/randomJob.js
@@ -1,31 +1,85 @@
-const jobs = [
-    process.env['PLD'], //   \:pld:
-    process.env['WAR'], //   \:war:
-    process.env['DRK'], //   \:drk:
-    process.env['GNB'], //   \:gnb:
+class Job {
+    #name;
+    #emojiId;
+    constructor(name, emojiId) {
+        this.#name = name;
+        this.#emojiId = emojiId;
+    }
+    GetName() {
+        return this.#name;
+    }
+    GetEmojiId() {
+        return this.#emojiId;
+    }
+}
+const categories = ['tank', 'healer', 'dps', 'melee', 'ranged', 'magic'];
+const tankPool = [
+    new Job('pld', '<:pld:1231445908604850186>'),
+    new Job('war', '<:war:1231446013919625286>'),
+    new Job('drk', '<:drk:1231446046471753738>'),
+    new Job('gnb', '<:gnb:1231446062443532298>')
+];
+const healerPool = [
+    new Job('whm', '<:whm:1231446129531682897>'),
+    new Job('sch', '<:sch:1231446142592614460>'),
+    new Job('ast', '<:ast:1231446155561271439>'),
+    new Job('sge', '<:sge:1231446168349839462>'),
+];
+const meleePool = [
+    new Job('mnk', '<:mnk:1231446284150509670>'),
+    new Job('drg', '<:drg:1231446257113890937>'),
+    new Job('nin', '<:nin:1231446297471615016>'),
+    new Job('sam', '<:sam:1231446338986708992>'),
+    new Job('rpr', '<:rpr:1231446310222172241>'),
+];
+const rangedPool = [
+    new Job('brd', '<:brd:1231446216751972434>'),
+    new Job('mch', '<:mch:1231446270543921222>'),
+    new Job('dnc', '<:dnc:1231446243973136445>'),
+];
+const magicPool = [    
+    new Job('blm', '<:blm:1231446229200797698>'),
+    new Job('smn', '<:smn:1231446351372357754>'),
+    new Job('rdm', '<:rdm:1231446326894530560>'),
+];
+const dpsPool = meleePool.concat(rangedPool, magicPool);
+const jobPool = tankPool.concat(tankPool, healerPool, dpsPool);
 
-    process.env['WHM'], //   \:whm:
-    process.env['SCH'], //   \:sch:
-    process.env['AST'], //   \:ast:
-    process.env['SGE'], //   \:sge:
-
-    process.env['MNK'], //   \:mnk:
-    process.env['DRG'], //   \:drg:
-    process.env['NIN'], //   \:nin:
-    process.env['SAM'], //   \:sam:
-    process.env['RPR'], //   \:rpr:
-
-    process.env['BRD'], //   \:brd:
-    process.env['MCH'], //   \:mch:
-    process.env['DNC'], //   \:dnc:
-
-    process.env['BLM'], //   \:blm:
-    process.env['SMN'], //   \:smn:
-    process.env['RDM'], //   \:rdm:
-]
-
-export function getRandomJob() {
-    var job = jobs[Math.floor(Math.random() * jobs.length)];
-    console.log(job);
-    return job;
+export function GetRandomJob(args = null) {
+    if (!args) {
+        return jobPool[Math.floor(Math.random() * jobPool.length)].GetEmojiId();
+    }
+    const params = args.toLowerCase().split('/');
+    for (let i = 0; i < params.length; i++) {
+        if(!jobPool.some(element => element.GetName() === params[i] || categories.includes(params[i]))) {
+            return null;
+        }
+    }
+    let pool = [];
+    params.forEach(element => {
+        switch(element) {
+            case 'tank':
+                pool = pool.concat(tankPool);
+                break;
+            case 'healer':
+                pool = pool.concat(healerPool);
+                break;
+            case 'dps':
+                pool = pool.concat(dpsPool);
+                break;
+            case 'melee':
+                pool = pool.concat(meleePool);
+                break;
+            case 'ranged':
+                pool = pool.concat(rangedPool);
+                break;
+            case 'magic':
+                pool = pool.concat(magicPool);
+                break;
+            default:
+                pool = pool.concat(jobPool.find((e) => e.GetName() === element));
+                break;
+        }
+    });
+    return pool[Math.floor(Math.random() * pool.length)].GetEmojiId();
 }
