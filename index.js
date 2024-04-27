f5ba5b77475486a4bc71b0c0b3d7c48addaaf5f6
Dustbowl <vuonganthony@hotmail.com>
2024-04-27 03:55:33 -0500

    updated error messages and added !job command options

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { Client, EmbedBuilder, IntentsBitField } from 'discord.js';
-import { getRandomJob } from './src/services/randomJob.js';
+import { Client, EmbedAssertions, EmbedBuilder, IntentsBitField } from 'discord.js';
+import { GetRandomJob } from './src/services/randomJob.js';
 import { parseForecastTime } from './src/services/frontlineForecast.js';
 import { ErrorEmbed } from './src/services/util.js';
 const client = new Client({
@@ -31,24 +31,49 @@ client.on('messageCreate', (message) => {
         message.reply({embeds: [helpEmbed]});
     }
 
-    if (message.content === '!job') {
-        var response = getRandomJob();
+    if (message.content.startsWith('!job')) {
+        const fields = message.content.split(' ');
+        if(fields.length > 2) {
+            message.reply({embeds: [ErrorEmbed('Error during !job. Ensure correct format!')]});
+            return;
+        }
+        if(fields.length == 2 ) {
+            var response = GetRandomJob(fields[1]);
+        } else {
+            var response = GetRandomJob();
+        }
+        if (response) {
+            message.reply(response);
+            return;
+        }
+        message.reply({embeds: [ErrorEmbed('Error during !job. Ensure correct format!')]});
+    }
+    /*     if (message.content === '!job') {
+        const response = GetRandomJob();
         if (response && typeof response === 'string') {
             message.reply(response);
         } else {
             console.log(`Error during !job response: ${response}`);
-            message.reply(ErrorEmbed('!job error'));
+            message.reply({embeds: [ErrorEmbed('!job error')]});
         }
-    }
+    }*/
 
     if(message.content.startsWith('!forecast')) {
         const fields = message.content.split(' ');
-        if(fields.length > 1) {
+        if(fields.length > 2) {
+            message.reply({embeds: [ErrorEmbed('Error during !forecast. Ensure correct format!')]});
+            return;
+        }
+        if(fields.length == 2 ) {
             var response = parseForecastTime(fields[1]);
         } else {
             var response = parseForecastTime();
         }
-        message.reply({embeds: [response]});
+        if (response) {
+            message.reply({embeds: [response]});
+            return;
+        }
+        message.reply({embeds: [ErrorEmbed('Error during !forecast. Ensure correct format!')]});
     }
 });
 
